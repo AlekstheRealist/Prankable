21282747e9401c4bae019e21579edf33cb3cacc0
Aleks Real <aleksanderreal@gmail.com>
2016-06-30 21:57:43 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Prankable::Application.config.secret_key_base = '7816cb8dd101740bfe311a8a3ce168e7bfa58ffe7476705d55b009e00e8e373b81f14614e3f6dff711d2abca1238749217e363acc8bf3f58afa34e02f516289f'
